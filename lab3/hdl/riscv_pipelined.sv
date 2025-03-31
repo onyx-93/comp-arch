7b45643a667d56a7cda369c8cf53be89b0cea4bf
onyx-93 <143036862+onyx-93@users.noreply.github.com>
2025-03-31 11:07:56 -0500

    srl fixed, original regfile re-installed

--- lab3/hdl/riscv_pipelined.sv
+++ lab3/hdl/riscv_pipelined.sv
@@ -418,20 +418,25 @@ module hazard(input  logic [4:0] Rs1D, Rs2D, Rs1E, Rs2E, RdE, RdM, RdW,
    assign FlushE = lwStallD | PCSrcE;
 endmodule
 
-module regfile (input logic         clk, 
-		input logic 	    we3, // RegWrite
-		input logic [4:0]   rs1, rs2, rd, // rs1 (register src1); rs2 (register src2); rd (destination register)
-		input logic [31:0]  wd3, // actual data written in register file
-		output logic [31:0] rd1, rd2); // rd1 reads rs1 = SrcA; rd2 reads rs2 = SrcB only for R-type operations (when ALUSrc is low)
-   
-   logic [31:0] 		    rf[31:0];
-   
-   always_ff @(posedge clk) // This block runs on the rising edge of clk, meaning it updates the registers synchronously.
-      if (we3 && rd!=0) rf[rd] <= wd3; // If write enable we3 is high, the register at address wa3 is updated with wd3. If we3 is low, no write occurs.
+module regfile(input  logic        clk, 
+               input logic 	   we3, 
+               input logic [ 4:0]  a1, a2, a3, 
+               input logic [31:0]  wd3, 
+               output logic [31:0] rd1, rd2);
 
-   assign rd1 = (rs1 == 5'b00000) ? 32'b0 : rf[rs1]; // If ra1 == 0, return 32'b0 (Register 0 always reads as zero). Otherwise, return the value stored in rf[ra1].
-   assign rd2 = (rs2 == 5'b00000) ? 32'b0 : rf[rs2]; // If ra2 == 0, return 32'b0 (Register 0 always reads as zero). Otherwise, return the value stored in rf[ra2].
-   
+   logic [31:0] 		   rf[31:0];
+
+   // three ported register file
+   // read two ports combinationally (A1/RD1, A2/RD2)
+   // write third port on rising edge of clock (A3/WD3/WE3)
+   // write occurs on falling edge of clock
+   // register 0 hardwired to 0
+
+   always_ff @(negedge clk)
+     if (we3) rf[a3] <= wd3;	
+
+   assign rd1 = (a1 != 0) ? rf[a1] : 0;
+   assign rd2 = (a2 != 0) ? rf[a2] : 0;
 endmodule // regfile
 
 module adder(input  [31:0] a, b,
